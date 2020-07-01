ba5c44befc64d6ee40d891975d1f0b5f7fff3dfa
Nicolas Coriale <nico7coriale@gmail.com>
2020-07-01 14:39:16 -0300

    finalizacion de la app

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="q-pa-md q-gutter-sm">
     <q-editor
+      v-if="!editionMode"
       v-model="editor"
       :definitions="{
         save: {
@@ -15,10 +16,28 @@
         ['upload', 'save']
       ]"
     />
+
+    <q-editor
+      v-else
+      v-model="editor"
+      :definitions="{
+        save: {
+          tip: 'Actualizar tarea',
+          icon: 'save',
+          label: 'Actualizar',
+          handler: updateWork
+        },
+      }"
+      :toolbar="[
+        ['bold', 'italic', 'strike', 'underline'],
+        ['upload', 'save']
+      ]"
+    />
     <q-card class="row" flat bordered v-for="(item, index) in tasks" :key="index">
       <q-card-section :class="item.state ? 'tachar' : ''" class="col" v-html="item.text" />
-      <q-btn class="q-mr-sm" color="green" @click="item.state = !item.state">Acción</q-btn>
-      <q-btn color="red" @click="deleteItem(index)">Eliminar</q-btn>
+      <!-- <q-btn class="q-mr-sm" color="green" @click="item.state = !item.state">Acción</q-btn> -->
+      <q-btn color="yellow-7 q-mr-sm" @click="edit(index, item.id)">Editar</q-btn>
+      <q-btn color="red" @click="deleteItem(index, item.id)">Eliminar</q-btn>
     </q-card>
     <div class="flex flex-center text-blue-grey-13" v-if="tasks.length == 0 ">
       <h4>Sin notas</h4>
@@ -27,48 +46,66 @@
 </template>
 
 <script>
-import { db } from 'boot/firebase'
+import { db } from "boot/firebase";
 export default {
   data() {
     return {
       editor: "",
-      tasks: []
+      tasks: [],
+      index: null,
+      editionMode: false,
+      id: null
     };
   },
   created() {
-    this.listTasks()
+    this.listTasks();
   },
   methods: {
     async listTasks() {
       try {
-        const resDB = await db.collection('tasks').get()
+        const resDB = await db.collection("tasks").get();
 
         resDB.forEach(res => {
-          console.log(res.id)
+          console.log(res.id);
           const task = {
             id: res.id,
             text: res.data().text,
-            state: res.data().state,
-          }
-          this.tasks.push(task)
-        })
+            state: res.data().state
+          };
+          this.tasks.push(task);
+        });
       } catch (error) {
-        console.log(error)
-      }
+        console.log(error);
+      } /* finally {
+        this.$q.loading.hide()
+      } */
     },
     async saveWork() {
-      this.tasks.push({
-        text: this.editor,
-        state: false
-      });
-      this.$q.notify({
-        message: "Tarea guardada",
-        color: "green-4",
-        textColor: "white",
-        icon: "cloud_done"
-      });
+      try {
+        const resDB = await db.collection("tasks").add({
+          text: this.editor,
+          state: false
+        });
+
+        this.tasks.push({
+          text: this.editor,
+          state: false,
+          id: resDB.id
+        });
+        this.editor = "";
+        this.$q.notify({
+          message: "Tarea guardada",
+          color: "green-4",
+          textColor: "white",
+          icon: "cloud_done"
+        });
+      } catch (error) {
+        console.log(error);
+      } /* finally {
+        this.$q.loading.hide()
+      } */
     },
-    deleteItem(index) {
+    deleteItem(index, id) {
       this.$q
         .dialog({
           dark: true,
@@ -77,9 +114,50 @@ export default {
           cancel: true,
           persistent: true
         })
-        .onOk(() => {
-          this.tasks.splice(index, 1);
+        .onOk(async () => {
+          try {
+            await db
+              .collection("tasks")
+              .doc(id)
+              .delete();
+            this.tasks.splice(index, 1);
+          } catch (error) {
+            console.log(error);
+          } /* finally {
+            this.$q.loading.hide()
+          } */
         });
+    },
+    edit(index, id) {
+      this.editionMode = true;
+      this.index = index;
+      this.id = id;
+      this.editor = this.tasks[index].text;
+    },
+    async updateWork() {
+      try {
+        const resDB = await db
+          .collection("tasks")
+          .doc(this.id)
+          .update({
+            text: this.editor
+          });
+        this.tasks[this.index].text = this.editor;
+
+        this.$q.notify({
+          message: "Tarea Actualizada",
+          color: "blue-3",
+          textColor: "white",
+          icon: "cloud_done"
+        });
+      } catch (error) {
+        console.log();
+      } finally {
+        this.editionMode = false;
+        this.index = null;
+        this.id = null;
+        this.editor = "";
+      }
     }
   }
 };
